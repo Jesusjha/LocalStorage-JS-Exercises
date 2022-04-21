f31f972a32d71af036cf960465998c46f3aa5dae
Jesus Herrera <heisusin@gmail.com>
2022-04-21 12:54:09 +0200

    exercise 03 solution

--- src/03-exercise/03-exercise.js
+++ src/03-exercise/03-exercise.js
@@ -1,5 +1,10 @@
 function exercise03() {
   // Complete the code of the function
+  localStorage.setItem('ex-03-city', 'Madrid');
+
+  document.getElementById('name').textContent = localStorage.getItem('ex-03-city');
+
+  localStorage.removeItem('ex-03-city');
 }
 
 export default exercise03;
