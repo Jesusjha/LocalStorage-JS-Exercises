1ad4cfe5993d054620103ab304b57ebe86559fc1
Jesus Herrera <heisusin@gmail.com>
2022-04-22 00:15:37 +0200

    exercise 05 solution

--- src/05-exercise/05-exercise.js
+++ src/05-exercise/05-exercise.js
@@ -1,5 +1,12 @@
 function exercise05() {
   // Complete the code of the function
+  let cityName = localStorage.getItem('ex-05-city');
+
+  if (cityName){
+    document.querySelector('#name').textContent = cityName;
+  }else 
+  localStorage.setItem('ex-05-city', 'Barcelona')
+  document.querySelector('#name').textContent = 'Barcelona';
 }
 
 export default exercise05;
