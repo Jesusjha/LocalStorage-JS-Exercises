fb7eb2887458882998f8096a4f8b3d9b922357b3
Jesus Herrera <heisusin@gmail.com>
2022-04-22 02:01:37 +0200

    exercise 06 solution

--- src/06-exercise/06-exercise.js
+++ src/06-exercise/06-exercise.js
@@ -1,5 +1,13 @@
 function exercise06() {
   // Complete the code of the function
+  let dataEx = JSON.parse(localStorage.getItem('ex-06-data'));
+  const cityNames = document.querySelector('.ex-list');
+
+  dataEx.forEach( (city) => {
+    let cityChild = document.createElement('li');
+    cityChild.textContent = city.name;
+    cityNames.appendChild(cityChild);
+  });
 }
 
 export default exercise06;
