032db3b86fe5c4c13c11edd9e3b3030328fc986c
Jesus Herrera <heisusin@gmail.com>
2022-04-22 02:31:31 +0200

    exercise 8 solution

--- src/08-exercise/08-exercise.js
+++ src/08-exercise/08-exercise.js
@@ -1,10 +1,14 @@
 function exercise08() {
-  let user = {
-    name: "mark",
-    age: 44,
-  };
+    let user = {
+        name: "mark",
+        age: 44,
+    };
 
-  // Complete the code of the function
+    // Complete the code of the function
+    let lastEx = JSON.parse(localStorage.getItem("ex-08-users"));
+    lastEx.push(user);
+    let lastString = JSON.stringify(lastEx);
+    localStorage.setItem("ex-08-users", lastString);
 }
 
 export default exercise08;
